d1bf0ccc3b0efc5ae9f49c799444d5698bbe3d46
Frederik Beyer <frbeyer@c2r8s3.42wolfsburg.de>
2024-07-04 18:34:49 +0200

    lol

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frbeyer <frbeyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 15:21:49 by frbeyer           #+#    #+#             */
+/*   Updated: 2024/07/04 18:34:09 by frbeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(void){
+}
+
+Account::~Account(void){
+}
+
+static int	getNbAccounts( void ){
+    
+}
+static int	getTotalAmount( void ){
+    
+}
+static int	getNbDeposits( void ){
+    
+}
+static int	getNbWithdrawals( void ){
+    
+}
+static void	displayAccountsInfos( void ){
+    
+}
+void	Account::makeDeposit( int deposit ){
+
+}
+bool	Account::makeWithdrawal( int withdrawal ){
+
+}
+int		Account::checkAmount( void ) const{
+
+}
+void	Account::displayStatus( void ) const{
+    
+}
+static void	_displayTimestamp( void ){
+    
+}
