1e2886680c0706438f6371d14675968d11b86193
Frederik Beyer <frbeyer@c2r8s1.42wolfsburg.de>
2024-08-07 14:39:18 +0200

    ex01

--- CPP04/ex01/Brain.hpp
+++ CPP04/ex01/Brain.hpp
@@ -5,13 +5,18 @@
 
 class Brain{
 
-    protected:
-    std::string ideas[100];
+    private:
+    std::string _ideas[100];
 
     public:
     Brain();
-    
-    ~Brain();
+    Brain(const Brain &copy);
+    virtual ~Brain();
+    Brain &operator=(const Brain &src);
+
+    const   std::string get_idea(size_t i)const;
+    const   std::string *idea_address(size_t i) const;
+    void    set_idea(size_t i, std::string idea);
 };
 
 #endif
--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -3,27 +3,14 @@
 #include "Dog.hpp"
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
+#include "Brain.hpp"
 
 int main()
 {
-const Animal* meta = new Animal();
 const Animal* j = new Dog();
-const WrongAnimal* i = new WrongCat();
-const Cat* cat = new Cat();
-std::cout << std::endl;
-std::cout << "type: " << j->getType()<< std::endl;
-std::cout << "type: " << i->getType()<< std::endl;
-std::cout << "type: " << cat->getType() << std::endl;
-std::cout << std::endl;
-std::cout << "Sounds: " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
-cat->makeSound();
-std::cout << std::endl;
-delete i;
+const Animal* i = new Cat();
+j->set_idea(0, "moin");
 delete j;
-delete meta;
-delete cat;
+delete i;
 return 0;
 }
