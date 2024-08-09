9be0a04ca82681d7b66d75713eaf281ce2796034
frbeyer1 <155981851+frbeyer1@users.noreply.github.com>
2024-08-09 15:51:32 +0000

    ex03

--- CPP04/ex03/AMateria.cpp
+++ CPP04/ex03/AMateria.cpp
@@ -4,4 +4,4 @@ AMateria::AMateria(){};
 AMateria::AMateria(std::string const & type){}; 
 AMateria::~AMateria(){};
 std::string const &AMateria::getType() const{};
-virtual void AMateria::use(ICharacter& target){}; 
+void AMateria::use(ICharacter& target){};
--- CPP04/ex03/AMateria.hpp
+++ CPP04/ex03/AMateria.hpp
@@ -5,7 +5,7 @@
 
 class AMateria {
     protected: 
-        
+
     public:
         AMateria();
         AMateria(std::string const & type); 
@@ -14,5 +14,15 @@ class AMateria {
         virtual AMateria* clone() const = 0; 
         virtual void use(ICharacter& target); 
 };  
+
+class ICharacter
+{
+    public: 
+        virtual ~ICharacter() {} 
+        virtual std::string const & getName() const = 0; 
+        virtual void equip(AMateria* m) = 0; 
+        virtual void unequip(int idx) = 0; 
+        virtual void use(int idx, ICharacter& target) = 0; 
+};
 
 #endif
