a02ae29202b8913d3e6962d3838536ef49121c28
frbeyer1 <155981851+frbeyer1@users.noreply.github.com>
2024-09-24 17:01:53 +0000

    ex00

--- Cpp05/ex00/Bureaucrat.hpp
+++ Cpp05/ex00/Bureaucrat.hpp
@@ -6,11 +6,36 @@
 class Bureaucrat{
 
     private:
-    std::string _name;
-    int _scrore;
+        const std::string _name;
+        int _grade;
+
+        void setGrade(int grade);
 
     public:
-    void GradeTooHighException();
-    void GradeTooLowException();
+        Bureaucrat();
+        Bureaucrat(const std::string name);
+        Bureaucrat(const std::string name, int grade);
+        Bureaucrat(int grade);
+        Bureaucrat(const Bureaucrat &src);
+        Bureaucrat &operator=(const Bureaucrat &src);
+        ~Bureaucrat();
+
+        void incrementGrade(void);
+        void decrementGrade(void);
+
+        const std::string getName(void) const;
+        size_t getGrade(void) const;
+
+        class GradeTooHighException: public std::exception{
+            public:
+            virtual const char *what() const throw();
+        };
+
+        class GradeTooLowException: public std::exception{
+            public:
+            virtual const char *what() const throw();
+        };
 };
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat *a);
 #endif
