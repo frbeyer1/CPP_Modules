c53c3d3f00084bb324b649f6de6409e6423a43e7
Frederik Beyer <frbeyer@c2r9s8.42wolfsburg.de>
2024-11-21 17:18:07 +0100

    ex02

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -10,11 +10,11 @@ class MutantStack : public std::stack<T>
 {
     public:
 
-        MutantStack() : std::stack<T>{
+        MutantStack() : std::stack<T>(){
             std::cout << "Default Constructor" << std::endl;
         };
 
-        MutantStack(const MutantStack &copy) : std::stack<T>;
+        MutantStack(const MutantStack &copy) : std::stack<T>()
         {
             std::cout << "Copy Constructor" << std::endl;
             *this = copy;
@@ -35,15 +35,15 @@ class MutantStack : public std::stack<T>
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
         iterator begin(){
-            return this->c.begin;
+            return this->c.begin();
         }
         iterator end(){
-            return this->c.end;
+            return this->c.end();
         }
         const_iterator begin() const{
-            return this->c.begin;
+            return this->c.begin();
         }
         const_iterator end() const{
-            return this->c.end;
+            return this->c.end();
         }
 };
