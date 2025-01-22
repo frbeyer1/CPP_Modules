2d3b78b134cbc50a173b5a0cd491e0075a2f3f4e
Frederik Beyer <frbeyer@c2r8s6.42wolfsburg.de>
2025-01-22 18:25:10 +0100

    btc finished

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,18 +6,21 @@
 #include <cstdlib>
 #include <string>
 #include <ctime>
-#include <cstring> 
+#include <cstring>
 
 class BitcoinExchange{
+    private:
+        std::multimap<std::string, float> _database;
+
     public:
         BitcoinExchange();
         BitcoinExchange(const BitcoinExchange &copy);
         ~BitcoinExchange();
         BitcoinExchange &operator=(const BitcoinExchange &src);
-        void readDatabase();
-        void readParse(char *argv);
-        void checkDate(std::string date);
-
-    private:
-        std::multimap<std::string, float> _database;
+        void    readDatabase();
+        void    saveValues(std::string data);
+        void    readInput(char *argv);
+        bool    checkDate(std::string date);
+        float   checkValue(std::string value);
+        void    calculate(std::string date, float value);
 };
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char **argv)
     {
         BitcoinExchange test;
         test.readDatabase();
-        test.readParse(argv[1]);
+        test.readInput(argv[1]);
+        // std::multimap<std::string, double>::iterator it;
+        // for (it = test._database.begin(); it != test._database.end(); ++it) {
+        //     std::cout << it->first << " => " << it->second << '\n';
+        // }
     }
     else
         std::cout << "Error: could not open file." << std::endl;
