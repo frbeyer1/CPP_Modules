95210105ba01075695a0e8a5870df4c98d4143e0
Frederik Beyer <frbeyer@c2r8s6.42wolfsburg.de>
2025-01-28 17:10:22 +0100

    ex02 plan

--- CPP09/ex02/src/PmergeMe.cpp
+++ CPP09/ex02/src/PmergeMe.cpp
@@ -42,9 +42,11 @@ int    PmergeMe::start_sorting(char **argv){
 - pairwise comparison of n/2 numbers, if odd ignore one element
 - sort n/2 larger numbers 
 - make main chain and pend chain
-- main chain already sorted with larger numbers named a1, a2, a3, a4, ...
+- check size if one element return
+- main chain with recursive binary sorted larger numbers named a1, a2, a3, a4, ...
+- if in a elemets get swap, swap pair elemts in b too
 - pend chain with unsorted numbers named b1, b2, b3, b4, ...
-- push first number of b chain if smaller than a1
-- generate jacobsthal numbers and turn b chain into this order for j <= n/2
-- then put b elements inbfornt of corresponding a elements, b2 infront a2
+- push b1 to a when a is sorted, because of pair treatment we know smallest of a is bigger element in then pair
+- generate jacobsthal numbers and push b elements according to the jn order for j <= n/2
+- for getting position of b element in a use the recursive binary search again
 */
