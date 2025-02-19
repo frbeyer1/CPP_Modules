a168235ab16cfa3788bfe34cacf22f9c6ca0baa7
Frederik Beyer <frbeyer@c2r9s8.42wolfsburg.de>
2025-02-19 21:14:14 +0100

    fix: fixed some issues in CPP06

--- CPP06/ex01/src/Serializer.cpp
+++ CPP06/ex01/src/Serializer.cpp
@@ -20,9 +20,29 @@ Data &Data::operator=(const Data &src){
     return *this;
 };
 
-uintptr_t serialize(Data *ptr){
+serializer::serializer(){
+    std::cout << "Serializer Constructor" << std::endl;
+};
+
+serializer::serializer(serializer &copy){
+    std::cout << "Serializer Copy Constructor" << std::endl;
+    *this = copy;
+};
+
+serializer::~serializer(){
+    std::cout << "Serializer Destructor" << std::endl;
+};
+
+serializer &serializer::operator=(const serializer &src){
+    std::cout << "ScalarConverter Assignment Operator" << std::endl;
+    if(this == &src)
+        return *this;
+    return *this;
+};
+
+uintptr_t serializer::serialize(Data *ptr){
     return(reinterpret_cast<uintptr_t>(ptr));
 };
-Data *deserialize(uintptr_t raw){
+Data *serializer::deserialize(uintptr_t raw){
     return(reinterpret_cast<Data *>(raw));
 };
